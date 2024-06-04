43195246bb95240dee3949318c0bdaf34d8d201c
Emilien Socchi <emilien.socchi@gmail.com>
2024-06-04 15:18:22 +0200

    Add file

--- .github/workflows/merge.sh
+++ .github/workflows/merge.sh
@@ -1,7 +1,7 @@
 #### Description #################################################################################
 #
 # Merges the content of all .json files in the repository containing cipher queries into a single
-# BloodHound-formated file.
+# BloodHound-formated file, and replaces custom BloodHound variables with specific content.
 #
 ####
 
@@ -18,27 +18,49 @@ do
     cat "$file" >> "$merged_file"
 done
 
+
 ##
 # Match and replace placeholders with content
 ##
-placeholder_azure_roles_tier_0='$VAR_ALL-AZ-ROLES-IN-T0'
-placeholder_entra_roles_tier_0='$VAR_ALL-ENTRA-ROLES-IN-T0'
-placeholder_entra_app_permissions_tier_0='$VAR_ALL-ENTRA-APP-PERMISSIONS-IN-T0'
-placeholder_azure_resources='$VAR_ALL-AZ-RESOURCES'
-
-tiering_dir="${repo_root_dir}/tiering/"
-tier_file_azure_roles="${tiering_dir}azure-roles.json"
+# Tiering placeholders
+placeholder_entra_roles_tier_0='_VAR_all-entra-roles-in-t0'
+placeholder_entra_app_permissions_tier_0='_VAR_all-entra-app-permissions-in-t0'
+placeholder_entra_app_permissions_tier_1='_VAR_all-entra-app-permissions-in-t1'
+placeholder_azure_roles_tier_0='_VAR_all-az-roles-in-t0'
+# Tiering files
+tiering_dir="${repo_root_dir}/variables/tiering/"
 tier_file_entra_roles="${tiering_dir}entra-roles.json"
 tier_file_entra_app_permissions="${tiering_dir}entra-application-permissions.json"
-
-azure_roles_tier_0="$(cat $tier_file_azure_roles | jq -r '.[] | select(.tier == "0" and .edgeName != "") | .edgeName' | sed -n ':a;N;${s/\n/|/g;p};ba')"
+tier_file_azure_roles="${tiering_dir}azure-roles.json"
+# Tiering content
 entra_roles_tier_0="$(cat $tier_file_entra_roles | jq -r '.[] | select(.tier == "0" and .edgeName != "") | .edgeName' | sed -n ':a;N;${s/\n/|/g;p};ba')"
 entra_app_permissions_tier_0="$(cat $tier_file_entra_app_permissions | jq -r '.[] | select(.tier == "0" and .edgeName != "") | .edgeName' | sed -n ':a;N;${s/\n/|/g;p};ba')"
+entra_app_permissions_tier_1="$(cat $tier_file_entra_app_permissions | jq -r '.[] | select(.tier == "1" and .edgeName != "") | .edgeName' | sed -n ':a;N;${s/\n/|/g;p};ba')"
+azure_roles_tier_0="$(cat $tier_file_azure_roles | jq -r '.[] | select(.tier == "0" and .edgeName != "") | .edgeName' | sed -n ':a;N;${s/\n/|/g;p};ba')"
 
+# Helper placeholders
+placeholder_all_security_principals='_VAR_all-security-principals'
+placeholder_all_azure_resources='_VAR_all-az-resources'
+placeholder_all_high_level_azure_scopes='_VAR_all-high-level-az-scopes'
+placeholder_all_azure_scopes='_VAR_all-az-scopes'
+
+# Helper file
+helper_dir="${repo_root_dir}/variables/"
+helper_file="${helper_dir}helper.json"
+# Helper content
+helper_all_security_principals="$(cat $helper_file | jq -r  '.[] | select(.variableName == '\"$placeholder_all_security_principals\"') | .components[]' | sed -n ':a;N;${s/\n/ or node:/g;p};ba')"
+helper_all_azure_resources="$(cat $helper_file | jq -r  '.[] | select(.variableName == '\"$placeholder_all_azure_resources\"') | .components[]' | sed -n ':a;N;${s/\n/ or node:/g;p};ba')"
+helper_all_high_level_azure_scopes="$(cat $helper_file | jq -r  '.[] | select(.variableName == '\"$placeholder_all_high_level_azure_scopes\"') | .components[]' | sed -n ':a;N;${s/\n/ or node:/g;p};ba')"
+helper_all_scopes="$(echo ${helper_all_high_level_azure_scopes} or node:${helper_all_azure_resources})"
+
+# Replace and merge content
 echo $merged_file \
-| sed "s/${placeholder_azure_roles_tier_0}/${azure_roles_tier_0}/" \
 | sed "s/${placeholder_entra_roles_tier_0}/${entra_roles_tier_0}/" \
 | sed "s/${placeholder_entra_app_permissions_tier_0}/${entra_app_permissions_tier_0}/" \
+| sed "s/${placeholder_entra_app_permissions_tier_1}/${entra_app_permissions_tier_1}/" \
+| sed "s/${placeholder_azure_roles_tier_0}/${azure_roles_tier_0}/" \
+| sed "s/${placeholder_all_azure_resources}/${helper_all_azure_resources}/" \
+| sed "s/${placeholder_all_azure_scopes}/${helper_all_scopes}/" \
 > $merged_file
 
 ##
